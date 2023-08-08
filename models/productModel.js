0685d003b59e52e3a4cddd8b41707a5dae0c48d1
Sulaiman Abid <sulaimanabid3@gmail.com>
2023-08-09 02:10:30 +0500

    add and get api

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter Product Name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+
+const app = express();
+
+//middleware
+app.use(express.json());
 
 //routes
 
-app.get('/', (req, res)=>{
-  res.send('hello node api')
-})
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({}); // curly braces means this will send all products from mongo
+    res.status(200).json(products);
+  } catch {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.listen(3000, ()=>{
-    console.log('API IS RUNNING ON PORT 3000')
-})
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sulaimanabid3:Salamander1999@cluster0.phqk2qg.mongodb.net/node-api"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+    app.listen(3000, () => {
+      console.log("API IS RUNNING ON PORT 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err, "error");
+  });
