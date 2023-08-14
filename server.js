b35dd8da6b70b525538a820cb65492b09b32119b
Sulaiman Abid <sulaimanabid3@gmail.com>
2023-08-14 22:11:26 +0500

    more apis

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
+app.use(express.urlencoded());
 
 //routes
 
@@ -19,12 +20,58 @@ app.get("/products", async (req, res) => {
   }
 });
 
-app.post("/products", async (req, res) => {
+app.post("/products/add", async (req, res) => {
+  // add
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/products/update/:id", async (req, res) => {
+  // update
+  try {
+    console.log(req, "request");
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: `cannot find the product` });
+    }
+    const updatedData = await Product.findById(id);
+    res.status(200).json(updatedData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products/:id", async (req, res) => {
+  // find by id
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/delete/:id", async (req, res) => {
+  // find by id
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (product) {
+      res
+        .status(200)
+        .json({ message: "Successfully deleted", deletedProduct: product });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch {
     res.status(500).json({ message: error.message });
   }
 });
